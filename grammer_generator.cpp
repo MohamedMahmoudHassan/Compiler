79e86cbe79b7ec66f1ea5b14f56c952c9b3d631c
Mohamed Mahmoud <mohamedmahmoudalihassan@gmail.com>
2018-12-21 14:35:23 +0200

    grammer rules - recognizing terminals

---
+++ grammer_generator.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include "analyser.h"
+
+struct node{
+    vector<string> v;
+
+    void add(string x){
+      v.push_back(x);
+    }
+
+};
+
+map<string, vector<node> > mp;
+map<string, bool> eps, trm;
+
+string term[] = {"identifier", "const", ";", "assOP", "OP", "dataType", "=", "while", "(", ")", "{", "}", "break", "continue", "if", "elseif", "else", "for", "return", "func"};
+string line, nonTrm;
+
+
+void grammer_genrator(){
+
+  //identifing terminals
+  for(string s : term)
+    trm[s] = 1;
+
+  //getting text that contains grammer
+  freopen("grammer", "r", stdin);
+
+  while(cin >> nonTrm){
+
+    if(nonTrm == "?")       //end of the file
+      break;
+
+    while(getline(cin, line)){
+      if(line == "")        //end of grammer of non-terminal
+        break;
+      if(line == "eps"){   //if this non-terminal accepts eps
+        eps[nonTrm] = 1;
+        continue;
+      }
+
+      istringstream s(line);
+      string word;
+
+      node new_node;      //each node contain one of the rules to this non-terminal
+      while(s >> word){
+        new_node.add(word);
+        cout << word << ' ' << trm[word] << endl;
+      }
+
+      mp[nonTrm].push_back(new_node);
+    }
+  }
+}
